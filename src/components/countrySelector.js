4614019f1722fccbda45964ab50cd609b059153a
Elliot Yandzio <elliotyandzio@gmail.com>
2020-02-10 18:03:10 +0000

    Initial commit with working react

---
+++ src/components/countrySelector.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CountrySelector extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Country Selector</h1>
+            </div>
+        )
+    }
+}
+
+export default CountrySelector;
