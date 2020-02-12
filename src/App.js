a358658857fe3e898b0b705b89d5c80c1dedb85e
Elliot Yandzio <elliotyandzio@gmail.com>
2020-02-12 16:16:43 +0000

    created buttons for each country and basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import CountrySelector from './components/countrySelector';
 
 function App() {
--- src/components/countrySelector.js
+++ src/components/countrySelector.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 
 class CountrySelector extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            countries: [
+                {
+                    name: 'United Kingdom',
+                    id: 'UK',
+                },
+                {
+                    name: 'United States',
+                    id: 'US',
+                },
+                {
+                    name: 'France',
+                    id: 'FR',
+                },
+                {
+                    name: 'Australia',
+                    id: 'AU',
+                },
+                {
+                    name: 'India',
+                    id: 'IN',
+                }
+            ]
+        };
+    }
+
     render() {
-        return (
+        let { countries } = this.state;
+        return ( 
             <div>
                 <h1>Country Selector</h1>
+                <div className="c-country-selector">
+                {countries.map(country => (
+                    <button key={country.id} id={country.id}>
+                        {country.name}
+                    </button>
+                ))}
+                </div>
             </div>
         )
     }
