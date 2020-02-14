5e6b228b3368b7ffa4e00e2cce57baa135c8f88b
Elliot Yandzio <elliotyandzio@gmail.com>
2020-02-14 15:08:55 +0000

    comments and tidying the app component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedCountryCode: '',
-      selectedCountryName: ''
+      selectedCountryCode: '', //used to store the country code that is returned when the user clicks a country
+      selectedCountryName: '' //used to store the country name that is returned when the user clicks a country
     }
   }
 
+  //callback function that is sent to the country selector component, stores the users country selection
+  //and adds it to the state in the component, ready to be passed by props to the new component.
   getSelectedCountry = (selectedCountryCode, selectedCountryName) => {
     this.setState({selectedCountryCode, selectedCountryName})
   }
